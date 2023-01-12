d68722bafbd0cda31a2df3254ae2dda9ed5937b4
Matt Krueger <mkrueger@rstms.net>
2023-01-11 22:38:23 -0600

    Bump version: 0.1.8 → 0.1.9

--- rstms_tq/version.py
+++ rstms_tq/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2023-01-11 22:37:15-06:00"
+__timestamp__ = "2023-01-11 22:38:23-06:00"
